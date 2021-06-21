54e26fb71060210f045eb0853b984d9f23d57dbb
lucasfranchini <lucasfranchini.a@gmail.com>
2021-06-21 19:21:09 -0300

    creating all the routes and starting a globalstyle

---
+++ src/Components/App.js
@@ -0,0 +1,32 @@
+import {BrowserRouter,Switch,Route} from "react-router-dom";
+import GlobalStyle from "../styles/GlobalStyle";
+import "../styles/reset.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <GlobalStyle/>
+        <Switch>
+          <Route path="/" exact>
+
+          </Route>
+          <Route path="/register" exact>
+
+          </Route>
+          <Route path="/transactions" exact>
+
+          </Route>
+          <Route path="/new-entry" exact>
+
+          </Route>
+          <Route path="/new-expense" exact>
+
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body {
+    background-color: #8C11BE;
+}
+`
+
+export default GlobalStyle;
