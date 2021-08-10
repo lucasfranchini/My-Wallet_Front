04201041140455cfc65f0df39a2716ba708bc67c
lucasfranchini <lucasfranchini.a@gmail.com>
2021-08-10 17:39:52 -0300

    style: Format signUpAndSignIn folder

--- src/Components/signUpAndSignIn/SignStyle.js
+++ src/Components/signUpAndSignIn/SignStyle.js
@@ -1,25 +1,25 @@
 import styled from "styled-components";
 
 const Body = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100vh;
-    a{
-        color:#fff;
-        font-size: 15px;
-        margin-top: 36px;
-    }
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  a {
+    color: #fff;
+    font-size: 15px;
+    margin-top: 36px;
+  }
+`;
 
 const Title = styled.h1`
-    font-family: 'Saira Stencil One';
-    color:#fff;
-    font-size: 32px;
-    line-height: 51px;
-    margin-bottom: 24px;
-`
+  font-family: "Saira Stencil One";
+  color: #fff;
+  font-size: 32px;
+  line-height: 51px;
+  margin-bottom: 24px;
+`;
 
-export {Body,Title}
+export { Body, Title };
--- src/Components/signUpAndSignIn/SignUp.js
+++ src/Components/signUpAndSignIn/SignUp.js
@@ -1,53 +1,78 @@
 import { useState } from "react";
-import { Body,Title } from "./SignStyle";
+import { Body, Title } from "./SignStyle";
 import Input from "../../styles/Input";
 import Button from "../../styles/Button";
 import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 
-export default function SignUp(){
-    const [body,setBody] = useState({
-        email: "",
-        name: "",
-        password: "",
-        confirmPassword: ""
-    })
-    const [load,setLoad] = useState(false);
-    const history = useHistory();
+export default function SignUp() {
+  const [body, setBody] = useState({
+    email: "",
+    name: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [load, setLoad] = useState(false);
+  const history = useHistory();
 
-    function register(e){
-        e.preventDefault();
-        setLoad(true);
-        if(body.password!==body.confirmPassword){
-            alert('password must be the same');
-            setLoad(false);
-        }
-        else{
-            delete body.confirmPassword;
-            const promise = axios.post('http://localhost:4000/sign-up',body);
-            promise.then(answer=>{
-                history.push('/');
-            })
-            promise.catch(()=>{
-                alert('error on sign Up, please try again');
-                setLoad(false);
-            })
-        }
-        
-        
+  function register(e) {
+    e.preventDefault();
+    setLoad(true);
+    if (body.password !== body.confirmPassword) {
+      alert("password must be the same");
+      setLoad(false);
+    } else {
+      delete body.confirmPassword;
+      const promise = axios.post("http://localhost:4000/sign-up", body);
+      promise.then((answer) => {
+        history.push("/");
+      });
+      promise.catch(() => {
+        alert("error on sign Up, please try again");
+        setLoad(false);
+      });
     }
+  }
 
-    return (
-        <Body>
-            <Title>MyWallet</Title>
-            <form onSubmit={register}>
-                <Input type="text" placeholder="Nome" value={body.name} onChange={e=>setBody({...body,name:e.target.value})} disabled={load}/>
-                <Input type="email" placeholder="E-mail" value={body.email} onChange={e=>setBody({...body,email:e.target.value})} disabled={load}/>
-                <Input type="password" placeholder="Senha" value={body.password} onChange={e=>setBody({...body,password:e.target.value})} disabled={load}/>
-                <Input type="password" placeholder="Confirme a senha" value={body.confirmPassword} onChange={e=>setBody({...body,confirmPassword:e.target.value})} disabled={load}/>
-                <Button type="submit" disabled={load}>Registrar</Button>
-            </form>
-            <Link to="/">Já tem uma conta? Entre agora!</Link>
-        </Body>
-    );
+  return (
+    <Body>
+      <Title>MyWallet</Title>
+      <form onSubmit={register}>
+        <Input
+          type="text"
+          placeholder="Nome"
+          value={body.name}
+          onChange={(e) => setBody({ ...body, name: e.target.value })}
+          disabled={load}
+        />
+        <Input
+          type="email"
+          placeholder="E-mail"
+          value={body.email}
+          onChange={(e) => setBody({ ...body, email: e.target.value })}
+          disabled={load}
+        />
+        <Input
+          type="password"
+          placeholder="Senha"
+          value={body.password}
+          onChange={(e) => setBody({ ...body, password: e.target.value })}
+          disabled={load}
+        />
+        <Input
+          type="password"
+          placeholder="Confirme a senha"
+          value={body.confirmPassword}
+          onChange={(e) =>
+            setBody({ ...body, confirmPassword: e.target.value })
+          }
+          disabled={load}
+        />
+        <Button type="submit" disabled={load}>
+          Registrar
+        </Button>
+      </form>
+      <Link to="/">Já tem uma conta? Entre agora!</Link>
+    </Body>
+  );
 }
